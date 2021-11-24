4455be15f46841361ad7fa7fcc14517182bdb89a
Sedra <83130573+siudro@users.noreply.github.com>
2021-11-24 17:02:29 +0300

    Update Fibonacci seq.cpp

--- Fibonacci seq.cpp
+++ Fibonacci seq.cpp
@@ -5,25 +5,23 @@ using namespace std;
 void*doit(void*);
 int i,size,fib[0];
 int main(){
-    cout<<"enter the fibbonacci sequence number:"<<endl;
+    cout<<"Enter the fibonacci sequence number: ";
     cin>>size;
     fib[size];
     pthread_t A;
     pthread_create(&A,NULL,doit,(void*)(intptr_t)size);
     pthread_join(A,NULL);
     for(i=0;i<size;i++)
-    cout<<fib[i]<<"";
+        cout<<fib[i]<<" , ";
 }
 void *doit(void* vptr){
     for(i=0;i<(intptr_t)vptr ;i++){
         if(i==0)
-        fib[i]=0;
+            fib[i]=0;
         else if (i==1)
-        fib[i]=1;
+            fib[i]=1;
         else
-        fib[i]=(fib[i-1]+fib[i-2]);
+            fib[i]=(fib[i-1]+fib[i-2]);
     }
     return (NULL);
 }
-
-
