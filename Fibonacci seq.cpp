6c31d4bf74fb345a80cd8ac97be084b5a70bb3ab
Sedra <83130573+siudro@users.noreply.github.com>
2021-11-24 14:42:50 +0300

    Update Fibonacci seq.cpp

--- Fibonacci seq.cpp
+++ Fibonacci seq.cpp
@@ -1,31 +1,27 @@
 #include <unistd.h>
 #include <iostream>
 #include <pthread.h>
-using std::cout;
-using std::cin;
-
+using namespace std;
 void*doit(void*);
-int i,size,seq[0];
+int i,size,fib[0];
 int main(){
-    cout<<"inter seq size:";
+    cout<<"enter the fibbonacci sequence number:"<<endl;
     cin>>size;
-    seq[size];
+    fib[size];
     pthread_t A;
     pthread_create(&A,NULL,doit,(void*)(intptr_t)size);
     pthread_join(A,NULL);
-    
-    COUT<<"\n";
     for(i=0;i<size;i++)
-    cout<<se[i]<<"";
+    cout<<fib[i]<<"";
 }
 void *doit(void* vptr){
     for(i=0;i<(intptr_t)vptr ;i++){
         if(i==0)
-        seq[i]=0;
+        fib[i]=0;
         else if (i==1)
-        seq[i]=1;
+        fib[i]=1;
         else
-        seq[i]=(seq[i-1]+seq[i-2]);
+        fib[i]=(fib[i-1]+fib[i-2]);
     }
     return (NULL);
 }
